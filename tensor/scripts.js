9795de03b4cfbcf72b074a0701b48a840a2a0185
Ypchen <ypchng@qq.com>
2019-12-19 17:30:14 +0800

    init

---
+++ tensor/scripts.js
@@ -0,0 +1,37 @@
+import * as tf from '@tensorflow/tfjs'
+
+const t0 = tf.tensor(1)
+
+t0.print()
+
+console.log(t0)
+
+const t1 = tf.tensor([1, 2, 3])
+
+t1.print()
+
+console.log(t1)
+
+const t2 = tf.tensor([
+  [1, 2, 34],
+  [3, 4, 2]
+])
+
+t2.print()
+
+console.log(t2)
+
+const t3 = tf.tensor([
+  [
+    [1, 2],
+    [3, 4]
+  ],
+  [
+    [5, 6],
+    [7, 8]
+  ]
+])
+
+t3.print()
+
+console.log(t3)
